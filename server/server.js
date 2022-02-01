56458260dcc72a205beeacdf5e69c313b18505ef
Arun Prajapati <arunkp1122@gmail.com>
2022-02-01 11:03:44 +0545

    Use dotenv only in development

--- server/server.js
+++ server/server.js
@@ -3,15 +3,21 @@ import compression from 'compression'
 import dbConnect from './app/utils/dbconnect.js'
 const app = express()
 
+if (process.env.NODE_ENV !== 'production') {
+   import dotenv from 'dotenv'
+   dotenv.config({
+      path: './.env',
+   })
+}
+
 dbConnect()
 app.use(compression())
 
-app.use('/', (req, res) => {
+app.use('/api/users', (req, res) => {
    res.send('hello lololosss')
 })
 
 const PORT = process.env.PORT || 8080
-
 app.listen(PORT, () => {
    console.log(`Server started at port :${PORT}`)
 })
